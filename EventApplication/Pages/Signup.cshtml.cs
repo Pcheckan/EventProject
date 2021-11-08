0b74dd5cbc598454b83b62d6ae4e2a52c8c443da
Preston <Prestoncheckan@gmail.com>
2021-11-08 16:13:18 -0500

    began work on forms signup

--- EventApplication/Pages/Signup.cshtml.cs
+++ EventApplication/Pages/Signup.cshtml.cs
@@ -5,8 +5,15 @@ namespace EventApplication.Pages
 {
     public class SignupModel : PageModel
     {
+
         public void OnGet()
         {
+        }
+        public void OnPost()
+        {
+            var firstname = Request.Form["firstName"];
+            var lastName = Request.Form["lastName"];
+            ViewData["confirmation"] = $"{firstname} {lastName}";
         }
     }
 }
